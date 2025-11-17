3290702377dba5bc9b035b3f7af113e2ef842ccb
Cédric Ronvel <cedric.ronvel@gmail.com>
2025-11-17 11:44:07 +0100

    Random number generators benchmark

--- bench/hash-bench.js
+++ bench/hash-bench.js
@@ -147,3 +147,59 @@ benchmark( 'Hash a 10KB random strings' , function() {
 } ) ;
 
 
+
+benchmark( 'Random Number Generators' , function() {
+	
+	var drng = new hash.DRNG() ,
+		topic = "player:bob,type:battle" ,
+		seed16 = hash.randomBase64( 16 ) ,
+		drng16 = new hash.DRNG( seed16 ) ,
+		drng16md5 = new hash.DRNG( seed16 , 'md5' ) ,
+		result ;
+	
+	competitor( 'Vanilla Math.random()' , function() {
+		result =
+			Math.random() + Math.random() + Math.random() + Math.random() + Math.random() +
+			Math.random() + Math.random() + Math.random() + Math.random() + Math.random() ;
+	} ) ;
+
+	competitor( 'Hash Kit Deterministic RNG: hash.DRNG() with default seed (32 bytes)' , function() {
+		result =
+			drng.random( topic ) + drng.random( topic ) + drng.random( topic ) + drng.random( topic ) + drng.random( topic ) +
+			drng.random( topic ) + drng.random( topic ) + drng.random( topic ) + drng.random( topic ) + drng.random( topic ) ;
+	} ) ;
+	
+	competitor( 'Hash Kit Deterministic RNG: hash.DRNG() with a 16 bytes seed' , function() {
+		result =
+			drng16.random( topic ) + drng16.random( topic ) + drng16.random( topic ) + drng16.random( topic ) + drng16.random( topic ) +
+			drng16.random( topic ) + drng16.random( topic ) + drng16.random( topic ) + drng16.random( topic ) + drng16.random( topic ) ;
+	} ) ;
+	
+	competitor( 'Hash Kit Deterministic RNG: hash.DRNG() with a 16 bytes seed and MD5 algo' , function() {
+		result =
+			drng16md5.random( topic ) + drng16md5.random( topic ) + drng16md5.random( topic ) + drng16md5.random( topic ) + drng16md5.random( topic ) +
+			drng16md5.random( topic ) + drng16md5.random( topic ) + drng16md5.random( topic ) + drng16md5.random( topic ) + drng16md5.random( topic ) ;
+	} ) ;
+	
+	competitor( 'Hash Kit hash.random(): using crypto.pseudoRandomBytes()' , function() {
+		result =
+			hash.random( false ) + hash.random( false ) + hash.random( false ) + hash.random( false ) + hash.random( false ) +
+			hash.random( false ) + hash.random( false ) + hash.random( false ) + hash.random( false ) + hash.random( false ) ;
+	} ) ;
+
+	competitor( 'Hash Kit hash.random(): using crypto.randomBytes()' , function() {
+		result =
+			hash.random( true ) + hash.random( true ) + hash.random( true ) + hash.random( true ) + hash.random( true ) +
+			hash.random( true ) + hash.random( true ) + hash.random( true ) + hash.random( true ) + hash.random( true ) ;
+	} ) ;
+
+	competitor( 'Baseline crypto.pseudoRandomBytes(4)' , function() {
+		crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ;
+		crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ; crypto.pseudoRandomBytes( 4 ) ;
+	} ) ;
+
+	competitor( 'Baseline crypto.pseudoRandomBytes(40)' , function() {
+		crypto.pseudoRandomBytes( 40 ) ;
+	} ) ;
+} ) ;
+
